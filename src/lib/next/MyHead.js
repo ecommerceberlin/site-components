207fb38e1ebbf49c3011ed59c013dcc746ac3116
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-01-28 13:39:02 +0100

    avoid duplicates in head

--- src/lib/next/MyHead.js
+++ src/lib/next/MyHead.js
@@ -39,36 +39,35 @@ const MyHead = ({
   //console.log(router.asPath);
 
   return (
+    
     <NextHead>
 
       <title>{tTitle}</title>
-      <meta name="description" content={tDescription} />
+      
+      <meta key="description" name="description" content={tDescription} />
 
 
       {/* <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
       <link rel="apple-touch-icon" href="/static/touch-icon.png" />
       <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
       <link rel="icon" href="/static/favicon.ico" /> */}
-      <meta property="og:url" content={prefixedUrl} />
-      <meta property="og:title" content={tTitle || ''} />
-      <meta property="og:description" content={tDescription} />
-      <meta name="twitter:site" content={prefixedUrl} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content={image} />
-      <meta property="og:image" content={image} />
+      
+      <meta key="og_url" property="og:url" content={prefixedUrl} />
+      <meta key="og_title" property="og:title" content={tTitle || ''} />
+      <meta key="og_description" property="og:description" content={tDescription} />
+      <meta key="twitter_site" name="twitter:site" content={prefixedUrl} />
+      <meta key="twitter_card" name="twitter:card" content="summary_large_image" />
 
-      <meta property="og:image:width" content={width} />
-      <meta property="og:image:height" content={height} />
+      <meta key="twitter_image" name="twitter:image" content={image} />
+      <meta key="og_image" property="og:image" content={image} />
 
-      <meta property="og:type" content="website" />
-      <meta property="fb:app_id" content={fb_appid} />
+      <meta key="og_image_width" property="og:image:width" content={width} />
+      <meta key="og_image_height" property="og:image:height" content={height} />
 
-  
+      <meta key="og_type" property="og:type" content="website" />
+      <meta key="og_fbappid" property="fb:app_id" content={fb_appid} />
 
-      <link rel="canonical" href={canonical(prefixedUrl)} />
-
-  
-
+      <link key="canonical" rel="canonical" href={canonical(prefixedUrl)} />
 
       {children}
     </NextHead>
@@ -86,7 +85,8 @@ MyHead.defaultProps = {
   description: '',
   descriptionLabel: 'event.opengraph.description',
 
-  image: "https://res.cloudinary.com/ecommerceberlin/image/upload/c_limit,w_1024/v1546943854/ebe_og_home.jpg",
+ // image: "", 
+  //"https://res.cloudinary.com/ecommerceberlin/image/upload/c_limit,w_1024/v1546943854/ebe_og_home.jpg",
   url: fullUrl('/')
 };
 
@@ -94,7 +94,7 @@ MyHead.propTypes = {
   //  title: string,
   description: string,
   url: string,
-  image: string
+ // image: string
 };
 
 const enhance = compose(
