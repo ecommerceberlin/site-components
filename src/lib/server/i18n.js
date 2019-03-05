360ada2b237ecdf5c306d764ec69f41e8640a90e
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-03-05 21:48:33 +0100

    added timeout and non 200 status handling for fetch operation

--- src/lib/server/i18n.js
+++ src/lib/server/i18n.js
@@ -4,14 +4,19 @@ const defaultTranslations = require("./translation.json");
 
 async function getTexts(translationUrl, cache, purge) {
 
-  return defaultTranslations;
-
   if (purge) {
     cache.del(translationUrl);
   }
 
   if (!cache.has(translationUrl)) {
-    const data = await fetch(translationUrl).then(response => response.json());
+    const data = await fetch(translationUrl, {timeout : 2000})
+    .then(response => {
+      if (response.status !== 200) {
+        return defaultTranslations;
+      }
+      return response.json();
+    });
+  
     cache.set(translationUrl, data);
    // console.log('API/texts not found! Fetching and caching...');
     return data;
