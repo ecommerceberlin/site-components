53c251ca5b1ec5a9e1459498d98611d1886653f0
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-04-02 00:22:06 +0200

    fixed cached texts

--- src/lib/server/i18n.js
+++ src/lib/server/i18n.js
@@ -2,6 +2,16 @@ const fetch = require('isomorphic-unfetch');
 const defaultTranslations = require("./translation.json");
 
 
+function handleErrors(response) {
+  if (!response.ok) {
+      throw Error(response.statusText);
+  }
+  return response;
+}
+
+
+
+
 async function getTexts(translationUrl, cache, purge) {
 
   if (purge) {
@@ -9,13 +19,8 @@ async function getTexts(translationUrl, cache, purge) {
   }
 
   if (!cache.has(translationUrl)) {
-    const data = await fetch(translationUrl, {timeout : 2000})
-    .then(response => {
-      if (response.status !== 200) {
-        return defaultTranslations;
-      }
-      return response.json();
-    });
+
+    const data = await fetch(translationUrl, {timeout : 2000}).then(handleErrors).then(response => response.json()).catch(error => defaultTranslations);
   
     cache.set(translationUrl, data);
    // console.log('API/texts not found! Fetching and caching...');
