06ab751c803d0bd8943c95fdea4f5fc4ef22049a
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-10-02 12:50:28 +0200

    show presenter instead of fname+lname when applicable

--- src/lib/components/Schedule/Presenter.js
+++ src/lib/components/Schedule/Presenter.js
@@ -6,25 +6,22 @@ import Typography from '../MyTypography';
 //import Tags from '../Tags';
 
 // <Tags tags={_get(company.profile, "keywords")} />
+import {getSpeakerName } from '../../helpers'
 
 const styles = {};
 
 const Presenter = ({ showBio, data }) => {
   const {
+
     bio,
     cname2,
-    fname,
-    lname,
     position,
-    presentation_description,
-    presentation_time,
-    presentation_venue,
-    presentation_title
+
   } = data;
 
   return (
     <div style={{ marginTop: 30 }}>
-      <Typography template="presenter1">{`${fname} ${lname}`}</Typography>
+      <Typography template="presenter1">{getSpeakerName(data)}</Typography>
 
       <Typography template="presenter2">{`${position} ${cname2}`}</Typography>
 
