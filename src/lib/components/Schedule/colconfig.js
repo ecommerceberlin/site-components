57cfcc091a822a5666f691ac5277c69fca130b06
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-01-06 00:41:22 +0200

    colconfig

--- src/lib/components/Schedule/colconfig.js
+++ src/lib/components/Schedule/colconfig.js
@@ -4,7 +4,7 @@ const venues = {
     1 : { xs: 12, sm: 12, md: 12, lg: 12, xl: 12 },
     2 : { xs: 12, sm: 6, md: 6, lg: 6, xl: 6 },
     3 : { xs: 12, sm: 6, md: 4, lg: 3, xl: 3 },
-    4 : { xs: 12, sm: 6, md: 3, lg: 3, xl: 3 },
+    4 : { xs: 12, sm: 6, md: 6, lg: 3, xl: 3 },
     5 : { xs: 12, sm: 6, md: 3, lg: 2, xl: 2 },
     6 : { xs: 12, sm: 6, md: 3, lg: 3, xl: 3 },
     7 : { xs: 12, sm: 6, md: 3, lg: 3, xl: 3 },
