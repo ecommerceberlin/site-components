566e22b0e2ad1c933229ad8d09965b1637550424
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-01-14 12:22:30 +0100

    use settings to feed RoleButtons

--- src/lib/components/FsButtons.js
+++ src/lib/components/FsButtons.js
@@ -130,20 +130,7 @@ function FsButtons(props) {
 FsButtons.defaultProps = {
   accent : "gold",
   first : false,
-  items : [
-    {
-      url: 'https://static.eventjuicer.com/photos/12961446_1288640741145929_7684227399478032531_o.jpg',
-      label: 'common.visitor',
-      width: '50%',
-      target : "/visit"
-    },
-    {
-      url: 'https://static.eventjuicer.com/photos/12967348_1288628734480463_3860331543127036065_o.jpg',
-      label: 'common.exhibitor',
-      width: '50%',
-      target : "/exhibit"
-    },
-  ]
+  items : []
 
 }
 
--- src/lib/compositions/WidgetRoleButtons.js
+++ src/lib/compositions/WidgetRoleButtons.js
@@ -8,22 +8,8 @@ const WidgetRoleButtons = (props) => (
 )
 
 WidgetRoleButtons.defaultProps = {
-    
-    items : [
-    {
-      url: 'https://static.eventjuicer.com/photos/12961446_1288640741145929_7684227399478032531_o.jpg',
-      label: 'common.visitor',
-      width: '50%',
-      onClick : "/visit"
-    },
-    {
-      url: 'https://static.eventjuicer.com/photos/12967348_1288628734480463_3860331543127036065_o.jpg',
-      label: 'common.exhibitor',
-      width: '50%',
-      target : "/exhibit"
-    },
-
-  ]
+    accent : "gold",
+    items : []
 
 }
 
