bbf97f0792e8d6760687bae0f266513d981ce137
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-07-28 18:46:10 +0200

    updated libs

--- src/lib/next/_app.js
+++ src/lib/next/_app.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 import Router from 'next/router';
-
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import JssProvider from 'react-jss/lib/JssProvider';
 import { getMuiContext } from '../material-ui';
-
 import { CHANGE_LOCALE, CHANGE_LOCALE_MSGS } from '../i18n';
-
 import { Provider } from 'react-redux';
-import createStore from '../redux';
-import withRedux from 'next-redux-wrapper';
-import withReduxSaga from 'next-redux-saga';
-
+import {wrapper} from '../redux';
 import fetch from 'isomorphic-unfetch';
-
 import {resourceFetchRequest} from '../components/redux'
-
 
 import * as gtag from '../services/gtag';
 
@@ -52,56 +43,56 @@ class MyApp extends App {
   }
 
 
-  static async getInitialProps({ Component, router, ctx }) {
+  // static async getInitialProps({ Component, router, ctx }) {
 
-    const { store, isServer, query, res } = ctx;
+  //   const { store, isServer, query, res } = ctx;
 
-    let texts, locale;
+  //   let texts, locale;
 
-    if (isServer) {
+  //   if (isServer) {
 
-      texts = 'texts' in res.locals ? res.locals.texts : {};
+  //     texts = 'texts' in res.locals ? res.locals.texts : {};
 
-      //validate locale!
-      if('locale' in res.locals)
-      {
-        store.dispatch({ type: CHANGE_LOCALE, locale: res.locals.locale });
-      }
+  //     //validate locale!
+  //     if('locale' in res.locals)
+  //     {
+  //       store.dispatch({ type: CHANGE_LOCALE, locale: res.locals.locale });
+  //     }
 
-    } else {
+  //   } else {
 
-      const res = await fetch('/_data/texts', {
-        headers: { Accept: 'application/json' }
-      });
+  //     const res = await fetch('/_data/texts', {
+  //       headers: { Accept: 'application/json' }
+  //     });
 
-      texts = await res.json();
-    }
+  //     texts = await res.json();
+  //   }
 
-    store.dispatch({ type: CHANGE_LOCALE_MSGS, messages: texts });
+  //   store.dispatch({ type: CHANGE_LOCALE_MSGS, messages: texts });
 
-    const componentInitialProps = Component.getInitialProps
-      ? await Component.getInitialProps(ctx)
-      : {};
+  //   const componentInitialProps = Component.getInitialProps
+  //     ? await Component.getInitialProps(ctx)
+  //     : {};
 
    
 
-    if("settings" in componentInitialProps)
-    {
-      store.dispatch({
-        type: "SETTINGS_SET",
-        payload : componentInitialProps.settings
-      })
-    }
+  //   if("settings" in componentInitialProps)
+  //   {
+  //     store.dispatch({
+  //       type: "SETTINGS_SET",
+  //       payload : componentInitialProps.settings
+  //     })
+  //   }
 
-    if("preload" in componentInitialProps)
-    {
-      store.dispatch(resourceFetchRequest(componentInitialProps.preload))
-    }
+  //   if("preload" in componentInitialProps)
+  //   {
+  //     store.dispatch(resourceFetchRequest(componentInitialProps.preload))
+  //   }
 
-    return {
-      pageProps: {...componentInitialProps}
-    };
-  }
+  //   return {
+  //     pageProps: {...componentInitialProps}
+  //   };
+  // }
 
 
   pageContext = null;
@@ -117,11 +108,20 @@ class MyApp extends App {
       gtag.pageview(url);
     })
 
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles && jssStyles.parentNode) {
-      jssStyles.parentNode.removeChild(jssStyles);
+
+
+    const style = document.getElementById('server-side-styles')
+
+    if (style) {
+      style.parentNode.removeChild(style)
     }
+
+
+    // // Remove the server-side injected CSS.
+    // const jssStyles = document.querySelector('#jss-server-side');
+    // if (jssStyles && jssStyles.parentNode) {
+    //   jssStyles.parentNode.removeChild(jssStyles);
+    // }
 
     const {pageProps, store} = this.props
 
@@ -133,14 +133,12 @@ class MyApp extends App {
 
   render() {
     const { Component, pageProps, store } = this.props;
+    const settings = Components.settings || {}
 
     return (
       <Container>
         {/* Wrap every page in Jss and Theme providers */}
-        <JssProvider
-          registry={this.pageContext.sheetsRegistry}
-          generateClassName={this.pageContext.generateClassName}
-        >
+      
           {/* MuiThemeProvider makes the theme available down the React
               tree thanks to React context. */}
           <MuiThemeProvider
@@ -156,10 +154,10 @@ class MyApp extends App {
               <Component pageContext={this.pageContext} {...pageProps} />
             </Provider>
           </MuiThemeProvider>
-        </JssProvider>
+      
       </Container>
     );
   }
 }
 
-export default withRedux(createStore)(withReduxSaga({ async: true })(MyApp));
+export default wrapper.withRedux(MyApp);
--- src/lib/redux/index.js
+++ src/lib/redux/index.js
@@ -1,5 +1,96 @@
-if (typeof window === 'undefined') {
-  module.exports = require('./store/server');
-} else {
-  module.exports = require('./store/client');
+
+import { useMemo } from 'react'
+import { applyMiddleware, createStore } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { createWrapper } from 'next-redux-wrapper'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import rootReducer from './reducers';
+import rootSaga from './sagas';
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['boothsSelected', 'app']
+};
+
+
+const bindMiddleware = (middleware) => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension')
+    return composeWithDevTools(applyMiddleware(...middleware))
+  }
+  return applyMiddleware(...middleware)
 }
+
+export const initStore = (preloadedState = {}) => {
+  const sagaMiddleware = createSagaMiddleware()
+  const store = createStore(
+                      persistCombineReducers(persistConfig, reducers), 
+                      preloadedState, 
+                      bindMiddleware([sagaMiddleware])
+    )
+
+  store.sagaTask = sagaMiddleware.run(rootSaga)
+
+
+  /**
+   * 
+   *  
+   * next-redux-saga depends on `runSagaTask` and `sagaTask` being attached to the store.
+   *
+   *   `runSagaTask` is used to rerun the rootSaga on the client when in sync mode (default)
+   *   `sagaTask` is used to await the rootSaga task before sending results to the client
+   */
+
+   /** 
+  _store.runSagaTask = () => {
+    _store.sagaTask = sagaMiddleware.run(rootSaga);
+  };
+
+  // run the rootSaga initially
+  _store.runSagaTask();
+
+   */
+
+  const persistor = persistStore(store, null, () => {
+    /* getNotified! */
+  });
+
+  return store
+}
+
+
+export const initializeStore = (preloadedState) => {
+  let _store = store ?? initStore(preloadedState)
+
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    })
+    // Reset the current store
+    store = undefined
+  }
+
+  // For SSG and SSR always create a new store
+  if (typeof window === 'undefined') return _store
+  // Create the store once in the client
+  if (!store) store = _store
+
+  return _store
+}
+
+
+// export function useStore(initialState) {
+//   const store = useMemo(() => initializeStore(initialState), [initialState])
+//   return store
+// }
+
+
+export const wrapper = createWrapper(initializeStore, { debug: true })
