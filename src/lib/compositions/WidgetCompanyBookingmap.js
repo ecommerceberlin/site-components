4eb2526913603e7ae13853d2bbc48ad78da8a4fd
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-09-29 22:44:55 +0200

    widgetcompanybookingmap updated to widgetsalesmap

--- src/lib/compositions/WidgetCompanyBookingmap.js
+++ src/lib/compositions/WidgetCompanyBookingmap.js
@@ -4,10 +4,13 @@ import { withStyles } from '@material-ui/core/styles';
 import { map, get } from 'lodash';
 import {Wrapper, Bookingmap} from '../components';
 import { getCompanyProfileInfo } from '../helpers';
+import Settings from '../datasources/Settings';
 
 const styles = {};
 
-const WidgetCompanyBookingmap = ({ company, label }) => {
+const WidgetCompanyBookingmap = ({disabled, steps, allowedGroupIds, boothStyleMapping, disabledTicketIds, company, label, ...wrapperProps}) => {
+  
+  
   const name = getCompanyProfileInfo(company, 'name');
   const purchases = get(company, 'instances');
   //const data = filterCompanyInstances(purchases, eventId);
@@ -20,17 +23,25 @@ const WidgetCompanyBookingmap = ({ company, label }) => {
     return null
   }
 
-  return (
-    <Wrapper
-      label={[label, {
-          cname2: name,
-          loc: selectedBoothNames.join(','),
-          smart_count: selectedBoothNames.length
-        }]}
-    >
-      <Bookingmap selected={selectedBoothIds} />
-    </Wrapper>
-  );
+  return (<Settings>{(get) => (
+        
+      <Wrapper {...wrapperProps} label={[label, {
+        cname2: name,
+        loc: selectedBoothNames.join(','),
+        smart_count: selectedBoothNames.length
+      }]}>
+
+        <Bookingmap 
+            disabled={disabled} 
+            disabledTicketIds={get("bookingmap.disabledTicketIds", disabledTicketIds)} 
+            height={ get("bookingmap.height") } 
+            boothStyleMapping={get("bookingmap.boothStyleMapping", boothStyleMapping)}
+            selected={selectedBoothIds} 
+        />
+
+        </Wrapper>
+       
+    )}</Settings>);
 };
 
 WidgetCompanyBookingmap.propTypes = {
@@ -39,7 +50,12 @@ WidgetCompanyBookingmap.propTypes = {
 };
 
 WidgetCompanyBookingmap.defaultProps = {
-  label: 'exhibitors.booth_location_full'
+  label: 'exhibitors.booth_location_full',
+  disabledTicketIds: [],
+  steps: [],
+  allowedGroupIds: [],
+  disabled : false,
+  boothStyleMapping: {}
 };
 
 export default withStyles(styles)(WidgetCompanyBookingmap);
