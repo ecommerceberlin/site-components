ae01522d70e51da024322d8b10c83a5b9603f0d8
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-11-20 13:56:39 +0100

    monitor number of updates

--- src/lib/helpers/hooks.js
+++ src/lib/helpers/hooks.js
@@ -1,14 +1,19 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
-import {resourceFetchSuccess} from '../components/redux/actions'
+import {resourceFetchSuccess, setUserToken} from '../components/redux/actions'
 import {getUserByToken} from './api'
 import get from 'lodash/get'
+import { useRouter } from 'next/router'
 
 export const useUserData = () => {
 
+    const [updates, setUpdates] = useState(0);
     const token = useSelector(state => state.app.token)
     const currentUser = useSelector(state => state.resources.currentUser)
+    const transactions = useSelector(state => state.transactions.forms)
+
+    const filteredTransactions = transactions.filter(item => item.action == "profile_updated")
     const settings = useSelector(state => state.settings)
     const dispatch = useDispatch();
 
@@ -21,14 +26,27 @@ export const useUserData = () => {
             dispatch(resourceFetchSuccess("currentUser", data));
         }
         
-        if(api && (!currentUser || !("token" in currentUser) || currentUser.token != token)){
+        if(api &&  (updates != filteredTransactions || !currentUser || !("token" in currentUser) || currentUser.token != token)){
             fetchTokenAndSetUser();
+            setUpdates(updates+1);
         }
       
-    }, [token])
+    }, [token, updates])
 
     return currentUser;
     
 }
 
+export const useToken = (token, target = "/account") => {
 
+    const router = useRouter();
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+    
+        if(token){
+            dispatch(setUserToken(token))
+            router.push(target)
+        }
+    })
+}
