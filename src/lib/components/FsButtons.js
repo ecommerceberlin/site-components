31682c87479141db8aded31193da1026230fbad6
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-03-11 23:31:41 +0100

    refactored RoleButtons

--- src/lib/components/FsButtons.js
+++ src/lib/components/FsButtons.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
 import RawTranslatedText from './RawTranslatedText'
 import Router from 'next/router'
+import {useSettings} from '../helpers'
 
-const styles = theme => ({
+
+const useStyles = makeStyles(theme => ({
+
   root: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -79,7 +81,8 @@ const styles = theme => ({
     left: 'calc(50% - 9px)',
     transition: theme.transitions.create('opacity'),
   },
-});
+
+}))
 
 const scrollTo = (to, as) => {
   if(typeof window !== 'undefined'){
@@ -87,56 +90,57 @@ const scrollTo = (to, as) => {
   }
 }
 
-function FsButtons(props) {
-  const { classes, items, first } = props;
+function FsButtons({ setting = "rolebuttons", ...props }) {
+
+  const settings = useSettings(setting)
+  const classes = useStyles()
+  const {items = [], first = false, accent = "gold"} = Object.assign({}, settings, props)
 
   return (
     <div className={classes.root} style={ first ? {marginTop: 10} : {}}>
-      {items.map(({label, url, width, target}) => (
-        <ButtonBase
-          focusRipple
-          key={label}
-          className={classes.image}
-          focusVisibleClassName={classes.focusVisible}
-          style={{
-            width: width,
-          }}
-          onClick={() => scrollTo(target)}
-        >
-          <span
-            className={classes.imageSrc}
+      {items.map(({label, url, width, target}) => {
+
+        if(!width){
+          width = `${100/items.length}%`
+        }
+
+        return (
+          <ButtonBase
+            focusRipple
+            key={label}
+            className={classes.image}
+            focusVisibleClassName={classes.focusVisible}
             style={{
-              backgroundImage: `url(${url})`,
+              width: width,
             }}
-          />
-          <span className={classes.imageBackdrop} />
-          <span className={classes.imageButton}>
-            <Typography
-              component="span"
-              variant="h5"
-              color="inherit"
-              className={classes.imageTitle}
-            >
-              <RawTranslatedText label={label} />
-              <span className={classes.imageMarked} />
-            </Typography>
-          </span>
-        </ButtonBase>
-      ))}
+            onClick={() => scrollTo(target)}
+          >
+            <span
+              className={classes.imageSrc}
+              style={{
+                backgroundImage: `url(${url})`,
+              }}
+            />
+            <span className={classes.imageBackdrop} />
+            <span className={classes.imageButton}>
+              <Typography
+                component="span"
+                variant="h5"
+                color="inherit"
+                className={classes.imageTitle}
+              >
+                <RawTranslatedText label={label} />
+                <span className={classes.imageMarked} />
+              </Typography>
+            </span>
+          </ButtonBase>
+        )
+
+      })}
     </div>
   );
 }
 
-FsButtons.defaultProps = {
-  accent : "gold",
-  first : false,
-  items : []
 
-}
 
-FsButtons.propTypes = {
-  classes: PropTypes.object.isRequired,
-  items : PropTypes.array.isRequired
-};
-
-export default withStyles(styles)(FsButtons);
+export default FsButtons
