d35735135e5489c2563d1db1314507eb263a325f
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-03-18 13:48:18 +0100

    new gtm embed

--- src/lib/next/MyHead.js
+++ src/lib/next/MyHead.js
@@ -53,17 +53,17 @@ const MyHead = ({
        
     <meta key="viewport" name="viewport" content='user-scalable=0, initial-scale=1, minimum-scale=1, width=device-width, height=device-height' />,
     // <meta key="theme_color" name="theme-color" content={theme.palette.primary.main} />,
-    <script key="gtm" name="gtm" async src={`https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GTM}`} />,
 
     <script
     key="gtmconfig"
     name="gtmconfig"
     dangerouslySetInnerHTML={{
     __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', '${process.env.NEXT_PUBLIC_GTM}');
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');
     `
     }}
     />,
