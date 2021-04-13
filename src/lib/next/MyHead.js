ad2ca4b9f62dad59ae60931b2074f64befe75262
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-04-14 00:26:06 +0200

    gtm moved to _document

--- src/lib/next/MyHead.js
+++ src/lib/next/MyHead.js
@@ -54,19 +54,7 @@ const MyHead = ({
     <meta key="viewport" name="viewport" content='user-scalable=0, initial-scale=1, minimum-scale=1, width=device-width, height=device-height' />,
     // <meta key="theme_color" name="theme-color" content={theme.palette.primary.main} />,
 
-    <script
-    key="gtmconfig"
-    name="gtmconfig"
-    dangerouslySetInnerHTML={{
-    __html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');
-    `
-    }}
-    />,
+  
     
     <meta key="charset" charSet="UTF-8" />,
     <meta key="google-translate" name="google" content="notranslate" />,
--- src/lib/services/gtag.js
+++ src/lib/services/gtag.js
@@ -1,10 +1,16 @@
 
+import isObject from 'lodash/isObject'
 /**
  * 
  * https://github.com/vercel/next.js/blob/canary/examples/with-google-tag-manager/lib/gtm.js
 */
 
 export const event = (event) => {
+
+  if(!isObject(event)){
+    return false
+  }
+
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push(event)
   //console.log("datalayer pushed!", event, window.dataLayer)
