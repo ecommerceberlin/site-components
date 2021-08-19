a0cc5b847de19340acd8b061430b9cb02f8a8f1f
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-08-19 12:51:11 +0200

    temp

--- src/lib/components/CartSync.js
+++ src/lib/components/CartSync.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { resourceFetchRequest, resourceFetchSuccess } from '../redux/actions';
-import { UserInteractedWith, CartItemsSelector } from '../../redux/selectors'
+import { getCart } from '../../redux/selectors'
 import Pusher from "pusher-js";
 
 
@@ -9,40 +9,40 @@ import Pusher from "pusher-js";
 const CartSync = ({autorefresh = 30}) => {
 
     const dispatch = useDispatch()
-    const interacted = useSelector((state) => UserInteractedWith(state, "bookingmap"))
-    const cartItems = useSelector(CartItemsSelector)
+    const cart = useSelector(getCart)
 
 
-    useEffect(() => {
 
-        if(!interacted && !cartItems){
-            return;
-        }
+    // useEffect(() => {
 
-        const pusher = new Pusher("ef91111f814df12adcef", {
-          cluster: "eu",
-        });
-        var channel = pusher.subscribe('eventjuicer');
-        channel.bind('NewLockWasCreated', function({data}){
-            dispatch(resourceFetchSuccess("blockings", data))
-            // dispatch(resourceFetchRequest(["formdata"]))
-        });
-        return () => {
-          pusher.unsubscribe("eventjuicer");
-        };
-    }, [interacted, cartItems]);
+    //     if(!interacted && !cartItems){
+    //         return;
+    //     }
 
-    useEffect(() => {
+    //     const pusher = new Pusher("ef91111f814df12adcef", {
+    //       cluster: "eu",
+    //     });
+    //     var channel = pusher.subscribe('eventjuicer');
+    //     channel.bind('NewLockWasCreated', function({data}){
+    //         dispatch(resourceFetchSuccess("blockings", data))
+    //         // dispatch(resourceFetchRequest(["formdata"]))
+    //     });
+    //     return () => {
+    //       pusher.unsubscribe("eventjuicer");
+    //     };
+    // }, [interacted, cartItems]);
 
-        if(!interacted && !cartItems){
-            return;
-        }
+    // useEffect(() => {
 
-        const interval = setInterval(() => {
-          dispatch(resourceFetchRequest(["formdata", "blockings"]))
-        }, autorefresh * 1000);
-        return () => clearInterval(interval);
-    }, [interacted, cartItems]);
+    //     if(!interacted && !cartItems){
+    //         return;
+    //     }
+
+    //     const interval = setInterval(() => {
+    //       dispatch(resourceFetchRequest(["formdata", "blockings"]))
+    //     }, autorefresh * 1000);
+    //     return () => clearInterval(interval);
+    // }, [interacted, cartItems]);
 
 
     return null
