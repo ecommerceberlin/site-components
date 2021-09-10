6c4ddc7940d4ace2d7656889103830dbb6522707
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-09-10 10:31:20 +0200

    votestatus overhaul (removed typography, etc) $10$

--- src/lib/components/VoteStatus.js
+++ src/lib/components/VoteStatus.js
@@ -1,9 +1,7 @@
 import React from 'react'
-//import PropTypes from 'prop-types';
 import compose from 'recompose/compose';
 import { withStyles } from '@material-ui/core/styles';
 import { translate } from '../i18n'
-import Typography from './MyTypography';
 
 const styles = {
     
@@ -16,13 +14,8 @@ const styles = {
 }
 
 const VoteStatus = ({all, enabled, classes, translate, max_votes}) => {
-
     const remaining = max_votes - all.length;
-
-    return enabled ? ( <Typography template="benefitsText">
-    {`${translate("common.remaining")}: ${remaining} ${translate("common.votes")}`}
-    </Typography>) : null
-
+    return enabled ? `${translate("common.remaining")} ${translate("common.votes")}: ${remaining} `: null
 }
 
 // all.map(item => (
