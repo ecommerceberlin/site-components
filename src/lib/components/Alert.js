45c25afa60b3153be2358ca144705d332dc9d65d
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-09-10 10:30:40 +0200

    making alert component more flexible $5$

--- src/lib/components/Alert.js
+++ src/lib/components/Alert.js
@@ -2,11 +2,11 @@ import React from 'react';
 import MuiAlert from '@material-ui/lab/Alert';
 import {useTranslate} from '../i18n'
 
-const Alert = ({type, label}) => {
+const Alert = ({type, label=null, content=null}) => {
 
     const [translate] = useTranslate();
 
-    return <MuiAlert severity={type}>{translate(label)}</MuiAlert>
+    return <MuiAlert severity={type}>{label && translate(label)}{content}</MuiAlert>
 }
 
 
