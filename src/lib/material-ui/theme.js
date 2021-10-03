8390a0cd3efc13e2ee57d64b0f99cecd9a9c557f
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-03 17:19:14 +0200

    updated base libs $45$

--- src/lib/material-ui/theme.js
+++ src/lib/material-ui/theme.js
@@ -1,8 +1,8 @@
  
-import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 import grey from '@material-ui/core/colors/grey';
 
+import { responsiveFontSizes, createTheme } from '@material-ui/core/styles';
 
 //https://material-ui.com/customization/default-theme/?expend-path=$.typography
 
@@ -45,6 +45,6 @@ export const defaultTheme = {
 };
 
 
-const createTheme = (theme) => responsiveFontSizes(createMuiTheme(theme));
+const createThemeFunc = (theme = defaultTheme) => responsiveFontSizes(createTheme(theme));
 
-export default createTheme;
+export default createThemeFunc;
