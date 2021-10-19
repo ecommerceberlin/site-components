f18e1985fba2d9cb9396c5c4f6c6e29ec90f72e9
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-19 05:31:41 +0200

    added more exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -13,26 +13,27 @@ export * from './redux'
 export * from './services'
 export * from './settings/redux/actions'
 export * from './heads'
-
 export { default as get } from 'lodash/get'
 export { default as isEmpty } from 'lodash/isEmpty'
 export { default as isObject } from 'lodash/isObject'
 export { default as isFunction } from 'lodash/isFunction'
 export { default as map } from 'lodash/map'
-
 export { default as Divider } from '@material-ui/core/Divider'
 export { default as Grid } from '@material-ui/core/Grid'
 export { default as Box } from '@material-ui/core/Box'
 export { default as Avatar } from '@material-ui/core/Avatar'
 export { default as TextField } from '@material-ui/core/TextField'
+export { default as Checkbox } from '@material-ui/core/Checkbox'
+export { default as FormControlLabel } from '@material-ui/core/FormControlLabel'
 export { makeStyles } from '@material-ui/core/styles';
 export { default as Typography } from '@material-ui/core/Typography'
 export { default as Button } from '@material-ui/core/Button'
 export { default as LinearProgress } from '@material-ui/core/LinearProgress'
 export { default as CircularProgress } from '@material-ui/core/CircularProgress'
-
 export { connect, useDispatch, useSelector } from 'react-redux'
 export { default as fetch } from 'isomorphic-unfetch'
 export { withRouter, useRouter } from 'next/router'
 export { default as YouTube } from 'react-player/youtube'
 export { default as Vimeo } from 'react-player/vimeo'
+
+
