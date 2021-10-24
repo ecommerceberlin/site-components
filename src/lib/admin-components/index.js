c822e82494215e58bffa16e365f0d0c2f8fa9f21
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-24 17:59:26 +0200

    removed admin components

--- src/lib/admin-components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Exhibitor } from './Exhibitor';
-export { default as ProfileErrors } from './ProfileErrors';
-export { default as Purchases } from './Purchases';
-
-
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,3 @@
-export * from './admin-components'
 export * from './components'
 export * from './compositions'
 export * from './datasources'
