324f6676575697f8592e4f00688972c262c702d8
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-12-03 01:42:47 +0100

    added cors middleware $30$

---
+++ src/lib/next/middleware.js
@@ -0,0 +1,26 @@
+import Cors from 'cors'
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+export function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export async function handleCors(req, res){
+
+    // Initializing the cors middleware
+    const cors = Cors({
+        methods: ['GET', 'HEAD'],
+    })
+
+    await runMiddleware(req, res, cors)
+}
+
