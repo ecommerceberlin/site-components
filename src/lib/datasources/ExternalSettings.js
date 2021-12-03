e37cea72cb2249c87ea4cf452822cb87a5fdb40e
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-12-03 01:44:55 +0100

    do not use internal proxy $5$

--- src/lib/datasources/ExternalSettings.js
+++ src/lib/datasources/ExternalSettings.js
@@ -7,18 +7,20 @@ const ExternalSettings = ({urls}) => {
     const dispatch = useDispatch()
 
     useEffect(() => {
-
-        if(urls && Array.isArray(urls)){
-            Promise.allSettled(urls.map(url => fetch(`/api/proxy?url=${ encodeURIComponent(url) }` ).then(response => response.json()))).then(data => {
-                const obj = {}
-                data.forEach((p, i)=> {
-                  if(p.status == "fulfilled"){
-                    obj[urls[i]] = p.value
-                  }
-                })
-                dispatch(setSettings(obj))
-            })
-        }
+      /***
+       * `/api/proxy?url=${ encodeURIComponent(url) }` 
+       */
+      if(urls && Array.isArray(urls)){
+          Promise.allSettled(urls.map(url => fetch(url).then(response => response.json()))).then(data => {
+              const obj = {}
+              data.forEach((p, i)=> {
+                if(p.status == "fulfilled"){
+                  obj[urls[i]] = p.value
+                }
+              })
+              dispatch(setSettings(obj))
+          })
+      }
         
     }, [JSON.stringify(urls)])
 
