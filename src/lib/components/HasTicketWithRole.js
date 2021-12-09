8d80410e9fae6eae829e2fc1744a19df4e3e6ac6
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-12-09 02:23:01 +0100

    Update HasTicketWithRole.js

--- src/lib/components/HasTicketWithRole.js
+++ src/lib/components/HasTicketWithRole.js
@@ -7,10 +7,10 @@ const fetcher = url => fetch(url).then(r => r.json())
 
 const HasTicketWithRole = ({email="", project=null, role="visitor", children = null}) => {
 
-    const {email} = useSelector(state => state.resources.currentLinkedinUser)
+    const profile = useSelector(state => state.resources.currentLinkedinUser)
 
     console.log(email)
-    
+
     // const { data, error } = useSWR(`https://api.eventjuicer.com/v1/public/${project || process.env.NEXT_PUBLIC_PROJECT}`, fetcher, { 
     //     refreshInterval: 60*1000, //pull every minute
     //     refreshWhenHidden: true 
