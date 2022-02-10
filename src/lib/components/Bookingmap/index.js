ab20543fd89b0d47ae88d6d25d1c803d2fe89fea
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-02-10 11:45:27 +0100

    export boothcontext

--- src/lib/components/Bookingmap/index.js
+++ src/lib/components/Bookingmap/index.js
@@ -6,16 +6,19 @@
 
 
 export { default as Bookingmap } from './Bookingmap';
-export { default as TicketBuyButton } from './TicketBuyButton';
-export { default as TicketPrice } from './TicketPrice';
-export { default as TicketRemainingInfo } from './TicketRemainingInfo';
-export { default as TicketDate } from './TicketDate';
-
-
-
 export { default as BookingmapLegend } from './BookingmapLegend';
 export { default as Booth } from './Booth';
 export { default as RecentPurchases } from './RecentPurchases';
 export { default as BoothOrderSteps } from './BoothOrderSteps';
 export { default as boothStyles, getStylingName } from './boothStyles';
+export { default as BoothContext, useBoothContext, useBookingmapSettings } from './Context';
+
+
+
+
+export { default as TicketBuyButton } from './TicketBuyButton';
+export { default as TicketPrice } from './TicketPrice';
+export { default as TicketRemainingInfo } from './TicketRemainingInfo';
+export { default as TicketDate } from './TicketDate';
+
 
