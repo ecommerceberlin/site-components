567812107f94a4362698c88b50eaf383c8a86436
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-21 21:47:17 +0100

    added presentation context

---
+++ src/lib/components/Schedule/context.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { VenueSelector } from './redux'
+import { useSettings, resizeCloudinaryImage} from '../../helpers'
+import size from 'lodash/size'
+
+const PresentationContextProvider = React.createContext({});
+
+export const usePresentation = () => {
+
+    const context = React.useContext(PresentationContextProvider)  
+
+    return context
+}
+
+
+export const PresentationContext = ({setting="", data={}, children}) => {
+  
+    const {times, venues, categories} = useSettings(setting)
+    const selectedVenue = useSelector(VenueSelector)
+    
+    const value = React.useMemo(()=> ({
+        categories,
+        times,
+        venues,
+        venues_count: size(venues),
+        selectedVenue, 
+
+        id: data.id,
+     
+        presenter: data.presenter,
+        company: data.cname2,
+        position: data.position,
+        bio: data.bio,
+        linkedin: data.profile_linkedin,
+
+        title: data.presentation_title,
+        description: data.presentation_description,
+        category: data.presentation_category,
+      
+        time: data.presentation_time,
+        venue: data.presentation_venue,
+     
+        avatar: resizeCloudinaryImage(data.avatar_cdn, 100, 100),
+        logotype: resizeCloudinaryImage(data.logotype_cdn, 200, 200)
+
+    }), [data, selectedVenue, times, venues, categories])
+    
+    return <PresentationContextProvider.Provider value={value}>{children}</PresentationContextProvider.Provider>
+  }
+
+
+
