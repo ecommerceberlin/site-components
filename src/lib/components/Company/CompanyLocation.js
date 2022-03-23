916b5713dd95d820d067a6ae8f3c6b09b1b3c813
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-23 12:50:44 +0100

    restyled company location

--- src/lib/components/Company/CompanyLocation.js
+++ src/lib/components/Company/CompanyLocation.js
@@ -9,15 +9,36 @@ import { dialogShow } from '../redux/actions';
 import isEmpty from 'lodash/isEmpty'
 import {Booth, Bookingmap} from '../Bookingmap'
 import { useTranslate } from '../../i18n';
+import {makeStyles} from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { grey } from '@material-ui/core/colors';
+
+const useStyles = makeStyles(theme => ({
+
+    root: {
+        maxWidth: 300,
+        padding: 20,
+        backgroundColor: grey[300],
+        marginBottom: 20
+      }
+      
+}))
+
+
+
 const CompanyLocation = () => {
 
     const {name, boothIds, boothNames, mapSetting} = useCompany()
     const [translate] = useTranslate()
     const dispatch = useDispatch();
+    const classes = useStyles()
+
 
     const translationProps = {
         cname2: name, 
-        loc: boothNames.join(','),
+        loc: boothNames.join(', '),
         smart_count: boothNames.length
     }
 
@@ -31,8 +52,23 @@ const CompanyLocation = () => {
         return null
     }
 
-    return (<Button onClick={handleClick} startIcon={<LocationOnIcon />} label="exhibitors.booth_location" labelProps={translationProps} variant="contained" />)
+    return (
+
+        <Paper className={classes.root}>
+            <Grid container alignItems='center' justifyContent='center' spacing={3} direction="column">
+                <Grid item>
+                    <Typography variant="h4">{boothNames}</Typography>
+                </Grid>
+                <Grid item>
+                <Button onClick={handleClick} startIcon={<LocationOnIcon />} label="exhibitors.map.exhibitor_location" variant="text" />
+                </Grid>
+            </Grid>
+        </Paper>
+
+    )
    
 }
+
+
 
 export default CompanyLocation
