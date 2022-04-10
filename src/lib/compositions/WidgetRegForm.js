0da980c53bdf7701ecf2f6e27e7eb96c0b687233
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-11 01:36:56 +0200

    custom widgetregform to handle vip registration $30$

--- src/lib/compositions/WidgetRegForm.js
+++ src/lib/compositions/WidgetRegForm.js
@@ -31,6 +31,7 @@ const defaultProps = {
   ],
   start: ['email'],
   rightShadowed: false,
+  raw: false
 };
 
 const useStyles = makeStyles(theme => ({
@@ -47,9 +48,33 @@ const useStyles = makeStyles(theme => ({
 
 }))
 
+
+
+const WidgetRegFormContainer = ({wrapperProps={}, right=null, rightShadowed, children}) => {
+
+  const classes = useStyles()
+
+  return (
+    <Wrapper {...wrapperProps}>
+      <Grid container spacing={3} justifyContent="space-between">
+        <Grid item xs={12} sm={12} md={right? 7: 12} lg={right? 7: 12} xl={right? 7: 12}>
+        <Centered>
+
+        {children}
+        </Centered>
+         
+        </Grid>
+        {right && <Grid item xs={12} sm={12} md={5} lg={5} xl={5}>
+          {isString(right)? <img src={ right } className={rightShadowed? classes.rightShadowed: classes.image}  /> : right}
+        </Grid> }
+      </Grid>
+    </Wrapper>)
+
+}
+
+
 const WidgetRegForm = ({setting, ...props}) => {
 
-    const classes = useStyles()
     const settings = useSettings(setting)
     const {post_api, service_api} = useSettings("system")
     const [translate, locale] = useTranslate()
@@ -69,6 +94,7 @@ const WidgetRegForm = ({setting, ...props}) => {
       token,
       wrapperProps,
       api,
+      raw,
       ...other
     } = Object.assign({}, defaultProps, settings, props)
 
@@ -79,36 +105,41 @@ const WidgetRegForm = ({setting, ...props}) => {
 
     const fieldsWithOptions = fields.map(field => "options" in field && options && field.options in options ? {...field, options: options[field.options]} : field)
     
+      if(raw){
+       return ( <StepForm
+        data={ data }
+        ticket_id={ ticket_id }
+        fields={ fieldsWithOptions }
+        start={ start }
+        template={ email_template }
+        api={ api || post_api }
+        role={ role }
+        locale={ locale }
+        {...other}
+      />  )
+      }
+
       return (
-        <Wrapper {...wrapperProps}>
-          <Grid container spacing={3} justifyContent="space-between">
-            <Grid item xs={12} sm={12} md={right? 7: 12} lg={right? 7: 12} xl={right? 7: 12}>
-            <Centered>
-
-               <StepForm
-                data={ data }
-                tickets={ cart }
-                ticket_id={ ticket_id }
-                fields={ fieldsWithOptions }
-                start={ start }
-                template={ email_template }
-                api={ api || post_api }
-                updateApi={ service_api }
-                role={ role }
-                token={ token }
-                locale={ locale }
-                uuid={ uuid }
-                {...other}
-              />
-
-            </Centered>
-             
-            </Grid>
-            {right && <Grid item xs={12} sm={12} md={5} lg={5} xl={5}>
-              {isString(right)? <img src={ right } className={rightShadowed? classes.rightShadowed: classes.image}  /> : right}
-            </Grid> }
-          </Grid>
-        </Wrapper>)
+          <React.Fragment>
+          <WidgetRegFormContainer wrapperProps={wrapperProps} right={right} rightShadowed={rightShadowed}>
+          <StepForm
+            data={ data }
+            tickets={ cart }
+            ticket_id={ ticket_id }
+            fields={ fieldsWithOptions }
+            start={ start }
+            template={ email_template }
+            api={ api || post_api }
+            updateApi={ service_api }
+            role={ role }
+            token={ token }
+            locale={ locale }
+            uuid={ uuid }
+            {...other}
+          />  
+          </WidgetRegFormContainer>
+          </React.Fragment>
+       )
 }
 
 
