4aabfc49c4ad1792f213060534afa17b3b2bea5e
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-09-14 00:14:45 +0200

    use usePresentation ctx

--- src/lib/compositions/WidgetVotable.js
+++ src/lib/compositions/WidgetVotable.js
@@ -1,33 +1,21 @@
 import React from 'react'
-import { MyHead } from '../next';
 import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
 import _get from 'lodash/get';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-//import { resourceFetchRequest } from '../components/redux'
-import Head from 'next/head'
-
 
 import {
     MyTypography as Typography,
-    TwoColsLayout as Section,
     Wrapper,
-    Presentation,
-    Presenter as PresenterName,
     Sharer,
-    MyAvatar as Avatar,
-   // Speaker
    KeywordSelect,
-   ProfileLogotype
   } from '../components';
 
-
 import CallForPapers from '../datasources/CallForPapers'
-
-import { 
-    getSpeakerAvatar,
- } from '../helpers';
-
+import { PresentationContext } from '../components/Schedule/context';
+import { PresenterData, LogotypeAndAvatar } from '../components/Presenter';
+ 
 const styles = theme => ({
 
     voteButtonContainer : {
@@ -49,87 +37,67 @@ const Votable = ({id, vote, status, asPath, classes, show_votes, ...rest}) => (
     <CallForPapers id={id}>{
 
         ({filtered, all, record}) => {
-
-            
+    
             return (
 
-            <React.Fragment>
-
-      
-
             <Wrapper first={true} {...rest}>
-            <Section
-            leftSize={5}
-            left={
-            <div
-            style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            marginTop: 20,
-            marginBottom: 20
-            }}
-            >
-            <Avatar src={getSpeakerAvatar(record)} minimal={false} grayscale={false} />
 
-            <ProfileLogotype data={record} />
 
-         
 
-            </div>
-            }
-            leftCentered={true}
-            right={
-            <div>
-            
-            <div className={classes.voteButtonContainer}>
-            <div>
-               {vote}
-            </div>
-            <div className={classes.voteInfo}>
 
-                <React.Fragment>
+<div className={classes.voteButtonContainer}>
+<div>
+{vote}
+</div>
+<div className={classes.voteInfo}>
 
-              {show_votes && <Typography template="presenter1">
-                Votes: {record.votes}
-                </Typography>}
+<React.Fragment>
 
-                  {status}
+{show_votes && <Typography template="presenter1">
+Votes: {record.votes}
+</Typography>}
 
-                {/* <Typography template="benefitsText" label="callforpapers.voting.rules.description" /> */}
-              
-                </React.Fragment>
+{status}
 
-            </div>
-            </div>
+{/* <Typography template="benefitsText" label="callforpapers.voting.rules.description" /> */}
 
-          
+</React.Fragment>
+
+
+</div>
+</div>
+
+
+
+
+
+
+            <PresentationContext data={record}>
+
+
+            <Grid container spacing={2}>
+                <Grid item md={3} sm={12} xs={12}>
+                <LogotypeAndAvatar />
+                </Grid>
+                <Grid item md={9} sm={12} xs={12}>
+                <PresenterData />
+                </Grid>
+            </Grid>
 
             <Divider />
-
-            { <Presentation
-            title={record.presentation_title}
-            description={record.presentation_description}
-            />
-            }
-            
+    
             <KeywordSelect  href={name => `/vote/categories/${name}`} keywords={[].concat( _get(record, 'presentation_category', "") )} />
-            
+
             <Divider />
-            
+                
             <Sharer url={asPath} />
 
-            <Divider />
-
-          <PresenterName data={record} />
           
 
-            </div>
-            }
-            />
+            </PresentationContext>
             </Wrapper>
 
-            </React.Fragment>
+         
         )
     }
 
