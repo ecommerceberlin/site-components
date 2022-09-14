84441dcbc807d3e2421a9b55fd798cf56a3f8425
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-09-14 22:18:28 +0200

    reworked widgetvotable

--- src/lib/compositions/WidgetVotable.js
+++ src/lib/compositions/WidgetVotable.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import _get from 'lodash/get';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -10,6 +11,8 @@ import {
     Wrapper,
     Sharer,
    KeywordSelect,
+   TwoColsLayout,
+   GetTicketLink
   } from '../components';
 
 import CallForPapers from '../datasources/CallForPapers'
@@ -32,7 +35,7 @@ const styles = theme => ({
 })
 
 
-const Votable = ({id, vote, status, asPath, classes, show_votes, ...rest}) => (
+const WidgetVotable = ({id, vote, status, asPath, classes, show_votes, ...rest}) => (
 
     <CallForPapers id={id}>{
 
@@ -40,8 +43,34 @@ const Votable = ({id, vote, status, asPath, classes, show_votes, ...rest}) => (
     
             return (
 
-            <Wrapper first={true} {...rest}>
+                <Wrapper first={true} {...rest}>
 
+                <PresentationContext data={record}>
+
+                <TwoColsLayout
+                  reverse={true}
+                  leftSize={8}
+                  left={        
+                    <Box>
+                      <LogotypeAndAvatar />
+                     
+                      <Box mt={3} mb={3}>
+                        <Grid container spacing={2} alignItems="center">     
+                          {/* <Grid item><KeywordSelect keywords={get(company, 'profile.keywords', [])} /></Grid> */}
+                          <Grid item></Grid>
+                        </Grid>
+                      </Box>
+                      <PresenterData />
+                      <Divider />
+                      <Sharer url={`/vote/${id}`} />       
+            
+                    </Box>
+                  }
+                  leftCentered={false}
+                  right={
+                    <Box>
+                      <Box mt={7} maxWidth={300}>
+                        
 
 
 
@@ -50,52 +79,27 @@ const Votable = ({id, vote, status, asPath, classes, show_votes, ...rest}) => (
 {vote}
 </div>
 <div className={classes.voteInfo}>
-
 <React.Fragment>
-
-{show_votes && <Typography template="presenter1">
-Votes: {record.votes}
-</Typography>}
-
+{show_votes && <Typography template="presenter1">Votes: {record.votes}</Typography>}
 {status}
-
-{/* <Typography template="benefitsText" label="callforpapers.voting.rules.description" /> */}
-
+{/* <Typography template="benefitsText" label="callforpapers.voting.rules.description" />  */}
 </React.Fragment>
-
-
 </div>
 </div>
 
 
 
+            
+                      </Box>
+                    </Box>
+                  }
+                />
+                </PresentationContext>
+                </Wrapper>
 
 
 
-            <PresentationContext data={record}>
 
-
-            <Grid container spacing={2}>
-                <Grid item md={3} sm={12} xs={12}>
-                <LogotypeAndAvatar />
-                </Grid>
-                <Grid item md={9} sm={12} xs={12}>
-                <PresenterData />
-                </Grid>
-            </Grid>
-
-            <Divider />
-    
-            <KeywordSelect  href={name => `/vote/categories/${name}`} keywords={[].concat( _get(record, 'presentation_category', "") )} />
-
-            <Divider />
-                
-            <Sharer url={asPath} />
-
-          
-
-            </PresentationContext>
-            </Wrapper>
 
          
         )
@@ -109,16 +113,22 @@ Votes: {record.votes}
 
 
 
-Votable.propTypes = {
+WidgetVotable.propTypes = {
     asPath : PropTypes.string.isRequired
 }
 
-Votable.defaultProps = {
+WidgetVotable.defaultProps = {
     vote : null,
     status : null,
     label : "callforpapers.list.title",
     show_votes: false
 }
 
-export default withStyles(styles)(Votable)
+export default withStyles(styles)(WidgetVotable)
 
+/**
+ * 
+ 
+<KeywordSelect  href={name => `/vote/categories/${name}`} keywords={[].concat( _get(record, 'presentation_category', "") )} />
+
+ */
