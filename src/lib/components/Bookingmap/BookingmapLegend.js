d228d6925abde60e454f0478913b6c85f139ee3e
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-03-14 00:00:07 +0100

    refactored bookingmap legend

--- src/lib/components/Bookingmap/BookingmapLegend.js
+++ src/lib/components/Bookingmap/BookingmapLegend.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { useSelector } from 'react-redux';
 import _get from 'lodash/get';
@@ -7,19 +8,18 @@ import {getTicketGroups} from '../../redux/selectors'
 import BookingmapLegendBooth from './BookingmapLegendBooth'
 import { useTranslate } from '../../i18n'
 import { useBookingmapSettings } from './Context';
+import ImageButton  from '../ImageButton';
 
 const useStyles = makeStyles(theme => ({
     root : {
-      display: 'flex',
-      maxWidth : 1000,
+      width : "90%",
+      maxWidth: 1200,
       margin: '10px auto 10px auto',
-      alignItems : 'center',
-      justifyContent : 'center'
     },
 
     description : {
         marginRight : 10,
-        maxWidth : 600,
+        maxWidth : 800,
     },
     
     groups : {
@@ -32,21 +32,32 @@ const useStyles = makeStyles(theme => ({
 
 const BookingmapLegend = ({setting, ...props}) =>  {
 
-    const {allowedGroupIds} = useBookingmapSettings(setting, props)
+    const {allowedGroupIds, venueLayout} = useBookingmapSettings(setting, props)
     const classes = useStyles()
     const [translate] = useTranslate()
     const ticketgroups = useSelector(getTicketGroups)
     const filtered = (ticketgroups || []).filter(tg => allowedGroupIds.includes(tg.id))
 
    return (
-        <div className={classes.root}>
-        <div className={classes.description}>
-        <Typography variant="subtitle1">{ translate("event.sales.pool.legend")}</Typography>
-        </div>
-        <div className={classes.groups}>
-        {filtered.map(tg => <BookingmapLegendBooth key={tg.name} setting={setting} g={tg.id} label={tg.name} />)}
-        </div>
-        </div>)
+        <Grid container className={classes.root} justifyContent="center" alignItems="center">
+         
+            <Grid item xl={7} lg={7} md={6}>
+            <div className={classes.description}>
+            <Typography variant="subtitle2">{ translate("event.sales.pool.legend")}</Typography>
+            </div>
+            <div className={classes.groups}>
+            {filtered.map(tg => <BookingmapLegendBooth key={tg.name} setting={setting} g={tg.id} label={tg.name} />)}
+            </div>
+            </Grid>
+
+            {venueLayout ? <Grid item xl={5} lg={5} md={6}>
+                
+                <ImageButton src={venueLayout} label="common.venuelayout" />
+
+            </Grid>: null}
+ 
+        </Grid>
+      )
 
 }
   
