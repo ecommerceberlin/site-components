c140eb72c79a94471782e2816e5224809e114f78
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-03-31 00:04:18 +0200

    widgetorganizervip

--- src/lib/components/Alert.js
+++ src/lib/components/Alert.js
@@ -2,16 +2,12 @@ import React from 'react';
 import MuiAlert from '@material-ui/lab/Alert';
 import {useTranslate} from '../i18n'
 
-const Alert = ({type, label=null, content=null}) => {
+const Alert = ({type="error", label=null, content=null}) => {
 
     const [translate] = useTranslate();
 
     return <MuiAlert severity={type}>{label && translate(label)}{content}</MuiAlert>
 }
 
-
-Alert.defaultProps = {
-    type: "error"
-}
 
 export default Alert;
--- src/lib/compositions/WidgetOrganizerVip.js
+++ src/lib/compositions/WidgetOrganizerVip.js
@@ -1,6 +1,6 @@
  
 import React from 'react';
-import { Typography, Box, Avatar } from '@material-ui/core';
+import { Typography, Box, Avatar, Grid } from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles'
 import {grey, red} from '@material-ui/core/colors'
 import Wrapper from '../components/Wrapper'
@@ -8,7 +8,12 @@ import { useRecord, useSettings } from '../helpers';
 import { useTranslate } from '../i18n'
 import WidgetCompanyInteraction from './WidgetCompanyInteraction'
 import WidgetRegForm from './WidgetRegForm';
-import { isObject } from 'lodash';
+import WidgetCompany from './WidgetCompany';
+import { 
+    CompanyContextProvider, 
+    CompanyLogotype 
+} from '../components/Company';
+import { isObject, get } from 'lodash';
 
 
 const useStyles = makeStyles(theme => ({
@@ -16,9 +21,10 @@ const useStyles = makeStyles(theme => ({
     icon: {
         color: "#ffffff",
         backgroundColor: red[500],
-        width: theme.spacing(10),
-        height: theme.spacing(10),
-        fontSize: theme.typography.pxToRem(35)
+        width: theme.spacing(12),
+        height: theme.spacing(12),
+        fontSize: theme.typography.pxToRem(40),
+        fontWeight: 900
     }
 }))
 
@@ -28,26 +34,52 @@ const Disabled = () => {
     return <Typography variant="h4">{translate("vipcodes.code-is-expired")}</Typography>
 }
 
+const DefaultAvatar = () => {
+    const classes = useStyles()
+    return (<Avatar className={classes.icon}>VIP</Avatar>)
+
+}
+
+const DefaultAvatarOrCompanyLogotype = ({slug_or_id}) => {
+    const company = useRecord("companies", slug_or_id)
+
+    if(company && "id" in company){
+        return (
+            <CompanyContextProvider data={company}>
+                   <Grid container spacing={3} alignItems="center">
+                    <Grid item>
+                        <CompanyLogotype /></Grid>
+                    <Grid item>
+                        <DefaultAvatar />
+                    </Grid>
+                    </Grid>
+            </CompanyContextProvider>
+        )
+    }
+
+    return <DefaultAvatar />
+}
+
 const WidgetOrganizerVip = ({setting="", code=""}) => {
     const {allowed, wrapperProps, label} = useSettings(setting)
     // const {should_be_expired, code, id, company} = useRecord("vipcodes", query && "vipcode" in query? query.vipcode: "")
     const classes = useStyles()
     const [translate] = useTranslate()
+    const {company, customLabel} =  get(allowed, `${code}`, {})
+
 
     if(!code || !allowed || !isObject(allowed) || !(code in allowed) || !isObject(allowed[code]) ){
         return (<Wrapper first><Disabled /></Wrapper>)
     }
 
-    const {company_id, cname} = allowed[code]
 
     return (<Wrapper {...wrapperProps}>
         <WidgetCompanyInteraction 
-            label={label}
+            label={customLabel || label}
             regFormLabel="vipcodes.lets-start"
             data={{code}}
             endpoint="/visitor-check"
-            cname={cname} 
-            icons={<Avatar className={classes.icon}>VIP</Avatar>}
+            icons={<DefaultAvatarOrCompanyLogotype slug_or_id={ company } />}
             render={ ( {email, status} ) => {
 
                 switch(status){
@@ -70,6 +102,7 @@ const WidgetOrganizerVip = ({setting="", code=""}) => {
 
             }}
         />
+
         </Wrapper>)
 }
  
