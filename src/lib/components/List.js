c4dc81aabd0d7f7575579c282a4186619d223069
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-04-04 14:00:38 +0200

    refactored list components

--- src/lib/components/List.js
+++ src/lib/components/List.js
@@ -5,8 +5,9 @@ import ListItem from '@material-ui/core/ListItem';
 import Divider from '@material-ui/core/Divider';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import isFunction from 'lodash/isFunction'
 import {useRouter} from 'next/router'
 
@@ -16,19 +17,19 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: '600ch',
     backgroundColor: theme.palette.background.paper,
   },
-  inline: {
-    display: 'inline',
-  },
+  button: {
+    margin: theme.spacing(1),
+  }
 }));
 
-function AlignItemsList({data, avatar, primary, secondary, link}) {
+function AlignItemsList({data, avatar, primary="", secondary="", link, renderListItemAsButton=true, linkButtonIcon=null, linkButtonText=""}) {
 
   const classes = useStyles()
   const {push} = useRouter()
 
   return (
     <List className={classes.root}>{data.map(item => (
-             <React.Fragment key={item.id}><ListItem alignItems="flex-start" button onClick={isFunction(link)? ()=>push(link(item)): undefined}>
+             <React.Fragment key={item.id}><ListItem alignItems="flex-start" button={renderListItemAsButton}  onClick={renderListItemAsButton && isFunction(link)? ()=>push(link(item)): undefined}>
              {avatar && isFunction(avatar)&& <ListItemAvatar>
                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
              </ListItemAvatar>}
@@ -36,6 +37,13 @@ function AlignItemsList({data, avatar, primary, secondary, link}) {
                primary={isFunction(primary) && primary(item)}
                secondary={isFunction(secondary) && secondary(item)}
              />
+            {!renderListItemAsButton &&  isFunction(link)?  <Button
+            onClick={ ()=>push(link(item)) }
+            variant="outlined"
+            color="secondary"
+            className={classes.button}
+            endIcon={React.isValidElement(linkButtonIcon)? React.cloneElement(linkButtonIcon): null}
+            >{linkButtonText}</Button> :null }
            </ListItem>
            <Divider variant="inset" component="li" />
            </React.Fragment>
