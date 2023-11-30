f5ed5275c03b19deba26ef801de9eaf78661ff20
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-11-30 15:57:16 +0100

    handle markdown

--- src/lib/formik/FormSuccess.js
+++ src/lib/formik/FormSuccess.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { MyTypography } from '../components';
+import { Markdown } from '../components';
+import { Box } from '@material-ui/core';
 
-const FormSuccess = ({baseLabel}) => (
-  <MyTypography label={`${baseLabel}.form.success`} template="alert" />
-);
+const FormSuccess = ({baseLabel}) => {
+  
+  return (
+    <Box p={2} m={2} mb={6} border={1} borderRadius={1}>
+    <Markdown big={true} label={`${baseLabel}.form.success`} />
+    </Box>
+  );
+
+
+} 
 
 FormSuccess.defaultProps = {
   baseLabel : "visitors"
