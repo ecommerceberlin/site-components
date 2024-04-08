25f86dced5c3f7183279d957258264dc57ecdf28
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-04-08 16:42:38 +0200

    video playback problem

--- src/lib/components/FsVideo.js
+++ src/lib/components/FsVideo.js
@@ -88,7 +88,7 @@ const FsVideo = ({ setting, ...props }) => {
   return (
       <section className={classNames(classes.container, classes[overlay])} style={{ backgroundImage: `url(${background})` }}>
       <div className={classes.overlay}>{children}</div>
-        <video autoPlay muted loop className={classes.video} poster={background}>
+        <video autoPlay muted loop playsinline className={classes.video} poster={background}>
           <source src={videoSrc} type="video/mp4" />
         </video>
       </section>
